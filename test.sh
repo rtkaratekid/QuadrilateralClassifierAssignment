d3a7e5c068155799e5e04cf4825c70654991863b
Tristan <mayfieldtristan@gmail.com>
2019-01-25 19:41:29 -0700

    files

---
+++ test.sh
@@ -0,0 +1,14 @@
+./main < input.txt > output.txt
+diff output.txt expectedOutput.txt
+error=$?
+if [ $error -eq 0 ]
+then
+echo "test passed"
+elif [ $error -eq 1 ]
+then
+echo "test failed"
+exit 1
+else
+echo "test didn't run properly"
+exit 1
+fi
